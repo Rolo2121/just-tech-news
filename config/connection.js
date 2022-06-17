d48de9a5754b9ef307dc98e62586b4069116e5ee
Jacob  Bauer <jacobbauer@Jacobs-Air.hitronhub.home>
2022-06-16 23:17:30 -0500

    created commits routes

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
+
 // create connection to our database, pass in your MySQL information for username and password
 const sequelize = new Sequelize('just_tech_news_db', 'root', 'password', {
   host: 'localhost',
